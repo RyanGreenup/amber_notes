28aeaa9a63777e59398388186cb42cc1bebce7e1
Ryan G <exogenesis@protonmail.com>
2025-03-22 17:30:34 +1100

    fix: Close drawer-content div in Svelte page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,6 +41,7 @@
   <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content flex flex-col">
     <!-- Page content here -->
+  </div>
   <div class="drawer-side">
     <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"
     ></label>
