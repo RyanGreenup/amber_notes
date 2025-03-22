0312e61ffdc8b1226d28c6a94ac4d64ffe43ef55
Ryan G <exogenesis@protonmail.com>
2025-03-22 17:27:46 +1100

    feat: Add SidebarList component for sidebar navigation

---
+++ src/components/sidebar/SidebarList.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+// Improve this so the list items can be passed in as variables AI!
+</script>
+
+<ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+  <!-- Sidebar content here -->
+  <li><a>Sidebar Item 1</a></li>
+  <li><a>Sidebar Item 2</a></li>
+</ul>
