30bf74e4f5ab1f8c36a65d3b292cd67ca8359657
Ryan G <exogenesis@protonmail.com>
2025-03-22 18:12:08 +1100

    refactor: Simplify navbar and remove sidebar drawer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import IconHamburger from "../components/icons/IconHamburger.svelte";
   import IconSidewaysHamburger from "../components/icons/IconSidewaysHamburger.svelte";
   import SidebarList from "../components/sidebar/SidebarList.svelte";
-  import { Folder } from "@lucide/svelte";
+  import { Folder, LucideSidebar, Menu } from "@lucide/svelte";
 
   let name = $state("");
   let greetMsg = $state("");
@@ -15,33 +15,25 @@
   }
 </script>
 
-<div class="navbar bg-base-100 shadow-sm fixed top-0 z-10 w-full">
+<div class="navbar bg-base-100 shadow-sm">
   <div class="flex-none">
-    <label for="my-drawer-2" class="btn btn-square btn-ghost drawer-button">
-      <IconHamburger />
-    </label>
+    <button class="btn btn-square btn-ghost">
+      <LucideSidebar />
+    </button>
   </div>
   <div class="flex-1">
     <a class="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div class="flex-none">
     <button class="btn btn-square btn-ghost">
-      <IconSidewaysHamburger />
+      <LucideSidebar />
     </button>
   </div>
 </div>
 
-<div class="drawer lg:drawer-open pt-16">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
-    <!-- Page content here -->
-  </div>
-  <div class="drawer-side">
-    <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"
-    ></label>
-    <SidebarList />
-  </div>
-</div>
+
+
+
 
 <main class="container">
   <h1>Welcome to Tauri + Svelte</h1>
