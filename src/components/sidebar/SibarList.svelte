9e130b135b4942ba1bf7ebd05ecd315c2a928ef5
Ryan G <exogenesis@protonmail.com>
2025-03-22 17:26:08 +1100

    feat: Add hamburger icons and navbar with drawer layout

---
+++ src/components/sidebar/SibarList.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+// How can I improve this so the items can be passed in AI!
+</script>
+
+<ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+  <!-- Sidebar content here -->
+  <li><a>Sidebar Item 1</a></li>
+  <li><a>Sidebar Item 2</a></li>
+</ul>
