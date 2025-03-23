4d00f09d7536ed94dec633384e4da7541b991ae4
Ryan G <exogenesis@protonmail.com>
2025-03-23 19:09:04 +1100

    feat: Add debug header to display isViewingBook state in Sidebar

--- src/components/sidebar/Sidebar.svelte
+++ src/components/sidebar/Sidebar.svelte
@@ -19,7 +19,7 @@
   // Default to root shelves
   let currentParentId = "root";
   let shelves = get_shelves(currentParentId);
-  
+
   // Track if we're viewing a book
   let isViewingBook = false;
 
@@ -28,9 +28,11 @@
     if (id) {
       currentParentId = id;
       shelves = get_shelves(id);
-      
+
       // Check if the selected item is a book
-      const selectedShelf = Object.values(shelves).find(shelf => shelf.id === id);
+      const selectedShelf = Object.values(shelves).find(
+        (shelf) => shelf.id === id,
+      );
       isViewingBook = selectedShelf?.type === ShelfType.BOOK;
     }
   });
@@ -41,6 +43,9 @@
     ? 'w-64'
     : 'w-0'} transition-all duration-300 overflow-hidden flex flex-col"
 >
+  <h1 class="prose">
+    {isViewingBook}
+  </h1>
 
   {#if isViewingBook}
     <SidebarTree note_list={rootNotes} />
