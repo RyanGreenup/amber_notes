e741aa8899a0f7a82b16a7569808e2a5ef6c3c4a
Ryan G <exogenesis@protonmail.com>
2025-03-23 17:38:59 +1100

    refactor: Update shelves data structure and rendering in SidebarList

--- src/components/sidebar/SidebarList.svelte
+++ src/components/sidebar/SidebarList.svelte
@@ -11,34 +11,24 @@
   import ShelfItem from "./ShelfItem.svelte";
 
   /* For debugging and dev */
-  export let deweyDecimalRoots = [
-    {
+  export let shelves = {
+    "000": {
       title: "000",
       description: "Computer science, information & general works",
     },
-    { title: "100", description: "Philosophy & psychology" },
-    { title: "200", description: "Religion" },
-    { title: "300", description: "Social sciences" },
-    { title: "400", description: "Language" },
-    { title: "500", description: "Science" },
-    { title: "600", description: "Technology" },
-    { title: "700", description: "Arts & recreation" },
-    { title: "800", description: "Literature" },
-    { title: "900", description: "History & geography" },
-  ];
+    "100": { title: "100", description: "Philosophy & psychology" },
+    "200": { title: "200", description: "Religion" },
+    "600": { title: "800", description: "Literature" },
+    "700": { title: "900", description: "History & geography" },
+  };
+  // This is a list of note categories that the user will click to choose view a category.
+  // Add a button to go up just style it, don't worry about behaviour AI!
 </script>
-
-<!-- <div class="p-4 flex justify-between items-center"> -->
-<!--   <h2 class="font-bold">Notes</h2> -->
-<!--   <button class="btn btn-sm btn-ghost btn-circle"> -->
-<!--     <Plus size={18} /> -->
-<!--   </button> -->
-<!-- </div> -->
 
 <ul class="list bg-base-100 rounded-box shadow-md">
   <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Shelves</li>
 
-  {#each deweyDecimalRoots as { title, description }}
-    <ShelfItem {title} {description} />
+  {#each Object.entries(shelves) as [id, { title, description }]}
+    <ShelfItem {id} {title} {description} />
   {/each}
 </ul>
